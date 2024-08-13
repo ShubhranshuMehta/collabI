30999acd05a6b4b3f43b3624afd982093c1dea28
Shubhranshu <shubhranshumehta@gmail.com>
2024-08-13 16:37:05 +0530

    homepage

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -2,11 +2,14 @@ import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
 import React from 'react'
 import { SignedIn, SignInButton, SignedOut, UserButton } from "@clerk/nextjs"
+import CollaborativeRoom from '@/components/CollaborativeRoom'
 
 const Documents = () => {
     return (
-        <div>
+        <div className='flex w-full flex-col items-center'>
+            <CollaborativeRoom
 
+            />
         </div>
     )
 }
